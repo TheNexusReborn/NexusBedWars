5aa725418c07fda4754cca18ccd1751048342f0d
Firestar311 <slaterandrew0398@gmail.com>
2026-01-07 22:52:53 -0600

    Initial setup for bedwars

---
+++ src/main/java/com/thenexusreborn/com/bedwars/BedWars.java
@@ -0,0 +1,10 @@
+package com.thenexusreborn.com.bedwars;
+
+import com.thenexusreborn.nexuscore.api.NexusSpigotPlugin;
+
+public class BedWars extends NexusSpigotPlugin {
+    
+    public void onEnable() {
+        
+    }
+}
