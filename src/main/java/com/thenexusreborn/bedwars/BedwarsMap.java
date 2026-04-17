7b5f10c54bab544d009132e794dcb03df54bdf41
Firestar311 <slaterandrew0398@gmail.com>
2026-04-16 21:43:41 -0500

    Added a comment for the planned data needed for a Bedwars Map

--- src/main/java/com/thenexusreborn/bedwars/BedwarsMap.java
+++ src/main/java/com/thenexusreborn/bedwars/BedwarsMap.java
@@ -3,6 +3,21 @@ package com.thenexusreborn.bedwars;
 import com.thenexusreborn.gamemaps.model.GameMap;
 
 public class BedwarsMap extends GameMap {
+    
+    /*
+        Teams: 
+            Player Spawn Position
+            Forge Bounds and Item Spawn Position
+            Color
+            Item Shop Location
+            Upgrade Shop Location
+            Island Bounds
+            Bed Location
+            Bed Facing
+        Emerald Gen Locations
+        Diamond Gen Locations
+     */
+    
     public BedwarsMap(String fileName, String name) {
         super(fileName, name);
     }
