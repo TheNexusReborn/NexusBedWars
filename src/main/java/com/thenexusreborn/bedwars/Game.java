3402cc3e594535c653e4f28e3414f58461b4157d
Firestar311 <slaterandrew0398@gmail.com>
2026-04-29 15:48:21 -0500

    Added a Game skeleton class

---
+++ src/main/java/com/thenexusreborn/bedwars/Game.java
@@ -0,0 +1,27 @@
+package com.thenexusreborn.bedwars;
+
+import com.stardevllc.starlib.objects.key.Key;
+import com.stardevllc.starlib.objects.key.Keyable;
+
+/**
+ * This represents a full game that stores and controls progression and things
+ */
+public class Game implements Keyable {
+    
+    private Key key;
+    
+    @Override
+    public Key getKey() {
+        return key;
+    }
+    
+    @Override
+    public void setKey(Key key) {
+        this.key = key;
+    }
+    
+    @Override
+    public boolean supportsSettingKey() {
+        return true;
+    }
+}
